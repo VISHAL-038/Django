7eadbb82fa16f18c59d8a7a9a0ae4694dc8c58f0
vishal-038 <vk067414@gmail.com>
2024-09-30 23:08:02 +0530

    "views and urls:day 2"

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myProject import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), 
+    path('about-us/', views.aboutUS),
 ]
